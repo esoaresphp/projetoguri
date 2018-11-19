de3045fc74969e2db8ef5086ce9fe5057b3d0c92
Usuario <Usuario@192.168.25.16>
2018-11-19 14:35:34 -0200

    TERCEIRO COMIT

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo " ARQUIVO MODIFICADO";
+        echo " ARQUIVO MODIFICADO 123";
         ?>
     </body>
 </html>
